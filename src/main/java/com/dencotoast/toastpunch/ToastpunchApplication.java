26bf1deb58e94bdf12135734b20156e54c04e841
sachindb789 <sdubey@dencofamily.com>
2025-06-10 18:51:01 +0530

    Initial commit

---
+++ src/main/java/com/dencotoast/toastpunch/ToastpunchApplication.java
@@ -0,0 +1,14 @@
+package com.dencotoast.toastpunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToastpunchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToastpunchApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/test/java/com/dencotoast/toastpunch/ToastpunchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dencotoast.toastpunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToastpunchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
